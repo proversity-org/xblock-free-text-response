e878ea668274ff94f799b230cdcae2ce55c1c512
Jose Antonio Gonzalez <antonio@proversity.org>
2017-04-25 11:36:51 +0200

    remove print

--- freetextresponse/freetextresponse.py
+++ freetextresponse/freetextresponse.py
@@ -281,7 +281,6 @@ class FreeTextResponse(EnforceDueDates, StudioEditableXBlockMixin, XBlock):
             field_info = self._make_field_info(field_name, field)
             if field_info is not None:
                 if field_name == 'block_id':
-                    print 'i am in the if'
                     field_info['value'] = xblockId
                 
                 context["fields"].append(field_info)
